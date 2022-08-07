a7e92a43b196889b2f2a8eb1247cd7b7cee068e2
carolcampelo <campelocg@gmail.com>
2022-08-07 09:54:56 -0300

    Final commit com correções

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
